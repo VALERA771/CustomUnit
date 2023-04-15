da7edff966f3a124e0184550871a5b94c5037874
VALERA771 <val.germanov@gmail.com>
2023-04-15 22:07:16 +0300

    Some new features

---
+++ CustomUnit/Commands/List.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using CommandSystem;
+using Exiled.Permissions.Extensions;
+
+namespace CustomUnit.Commands;
+
+public class List : ICommand
+{
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+    {
+        if (!sender.CheckPermissions("cu.list", out response))
+            return false;
+
+        StringBuilder sb = new();
+
+        foreach (var name in Plugin.Configs.Keys)
+            sb.AppendLine(name);
+
+        response = $"All loaded units:\n{string.Join(" ", sb)}";
+        return true;
+    }
+
+    public string Command { get; }
+    public string[] Aliases { get; }
+    public string Description { get; }
+}
